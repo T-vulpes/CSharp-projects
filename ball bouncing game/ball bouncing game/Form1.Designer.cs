bb7807b072a8a796f4da1e2bb25943f682fa220c
Deniz <167799743+T-vulpes@users.noreply.github.com>
2024-10-06 17:04:16 +0300

    Add files via upload

---
+++ ball bouncing game/ball bouncing game/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace ball_bouncing_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.girlpic = new System.Windows.Forms.PictureBox();
+            this.balpic = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.girlpic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.balpic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(77, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(254, 72);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(77, 148);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(254, 72);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "STOP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // girlpic
+            // 
+            this.girlpic.Image = ((System.Drawing.Image)(resources.GetObject("girlpic.Image")));
+            this.girlpic.Location = new System.Drawing.Point(414, 18);
+            this.girlpic.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.girlpic.Name = "girlpic";
+            this.girlpic.Size = new System.Drawing.Size(278, 352);
+            this.girlpic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.girlpic.TabIndex = 2;
+            this.girlpic.TabStop = false;
+            // 
+            // balpic
+            // 
+            this.balpic.BackColor = System.Drawing.Color.Transparent;
+            this.balpic.Image = ((System.Drawing.Image)(resources.GetObject("balpic.Image")));
+            this.balpic.Location = new System.Drawing.Point(591, 220);
+            this.balpic.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.balpic.Name = "balpic";
+            this.balpic.Size = new System.Drawing.Size(71, 69);
+            this.balpic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.balpic.TabIndex = 3;
+            this.balpic.TabStop = false;
+            this.balpic.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(754, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 29);
+            this.label1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 260);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(301, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Number of bounced balls : ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(88, 302);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(148, 26);
+            this.textBox2.TabIndex = 7;
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Location = new System.Drawing.Point(722, 20);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(0, 20);
+            this.labelTime.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.OliveDrab;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(319, 351);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(471, 19);
+            this.textBox1.TabIndex = 9;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(864, 455);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.balpic);
+            this.Controls.Add(this.girlpic);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Ball Bouncing Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.girlpic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.balpic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox girlpic;
+        private System.Windows.Forms.PictureBox balpic;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ ball bouncing game/ball bouncing game/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ball_bouncing_game
+{
+    public partial class Form1 : Form
+    {
+        private int bounceCount = 0;  // Counter for the number of bounces
+        private int ballSpeed = 8;     // Speed at which the ball moves
+        private bool isFalling = true; // Flag to check if the ball is falling
+        private bool isGameStarted = false; // To track if the game has started
+        private bool isGameOver = false; // To check if the game is over
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            balpic.Visible = false;  // Hide the ball until the game starts
+            label1.Text = "Press SPACE to start";  // Initial message
+            textBox2.Text = "0";  // Clear the bounce count display
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // If the game is over, do not move the ball
+            if (isGameOver) return;
+
+            // If the ball is falling
+            if (isFalling)
+            {
+                balpic.Top += ballSpeed;  // Move the ball downwards
+
+                // If the ball reaches the bottom of the girl picture, stop the game
+                if (balpic.Top >= girlpic.Bottom - balpic.Height)
+                {
+                    timer1.Stop();  // Stop the timer
+                    label1.Text = $"Game Over! The ball bounced {bounceCount} times.";
+                    isGameStarted = false;  // The game has ended
+                    isGameOver = true;  // Activate game over check
+                    return;
+                }
+            }
+            else
+            {
+                balpic.Top -= ballSpeed;  // Move the ball upwards
+
+                // If the ball reaches the top limit, let it fall back
+                if (balpic.Top <= 50)  // Set the upper limit
+                {
+                    isFalling = true;
+                }
+            }
+        }
+
+        // Spacebar key press event handler
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Space && isGameStarted && !isGameOver)
+            {
+                bounceCount++;  // Increment bounce count
+                textBox2.Text = bounceCount.ToString();  // Update the bounce count display
+
+                isFalling = false;  // Reverse the ball's direction
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Start the game
+            isGameStarted = true;
+            isGameOver = false;  // Ensure the game is not marked as over when starting again
+            bounceCount = 0;  // Reset bounce count
+            balpic.Visible = true;  // Show the ball
+            balpic.Top = 50;  // Set initial position
+            isFalling = true;  // Start the ball falling
+            timer1.Start();  // Start the timer
+            label1.Text = "";  // Clear the on-screen message
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Stop the game
+            timer1.Stop();
+            balpic.Visible = false;  // Hide the ball
+            label1.Text = $"Game Stopped. You bounced the ball {bounceCount} times.";
+            isGameStarted = false;
+            isGameOver = true;  // Mark the game as over when stopped
+        }
+    }
+}
