e32be180c8b79465dcc6d3fd56713ccc090b7023
Deniz <167799743+T-vulpes@users.noreply.github.com>
2024-10-07 13:40:46 +0300

    Program.cs

--- printing numbers between two numbers/ConsoleApplication5/Program.cs
+++ printing numbers between two numbers/ConsoleApplication5/Program.cs
@@ -9,28 +9,22 @@ namespace ConsoleApplication5
     {
         static void Main(string[] args)
         {
-            /*klavyeden girilen 2 sayının arasında kalan sayıları
-             alt alta yazdıran program */
-            Console.WriteLine("klavyeden girilen 2 sayının arasında ki sayıları alt alta yazdıran program!");
+            Console.WriteLine("Program that prints numbers between two numbers entered from the keyboard one under the other!");
             Console.WriteLine("------------------------------------------------------------------------------------");
             int sayı1, sayı2;
-            Console.Write("birinci sayıyı giriniz:");
+            Console.Write("Enter the first number:");
             sayı1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("ikinci sayıyı giriniz:");
+            Console.Write("Enter the second number:");
             sayı2 = Convert.ToInt32(Console.ReadLine());
             if (sayı1 > sayı2)
                 for (int a = sayı1; a >= sayı2; a--)
                 {
                     Console.WriteLine(a);
-
-
                 }
             else
                 for (int b = sayı2; b >= sayı1; b--)
                 {
                     Console.WriteLine(b);
-
-
                 }
             Console.ReadKey();
         }
