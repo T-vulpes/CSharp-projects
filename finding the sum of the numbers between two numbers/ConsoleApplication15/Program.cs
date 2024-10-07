434d30cb55ac5317c8691196f12a3bcdf6350ea2
Deniz <167799743+T-vulpes@users.noreply.github.com>
2024-10-07 13:36:46 +0300

    Program.cs

--- finding the sum of the numbers between two numbers/ConsoleApplication15/Program.cs
+++ finding the sum of the numbers between two numbers/ConsoleApplication15/Program.cs
@@ -9,40 +9,30 @@ namespace ConsoleApplication15
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("klavyeden girilen 2 sayının arasında ki sayıların toplamını yazdıran program!");
+            Console.WriteLine("Program that prints the sum of two numbers entered from the keyboard!");
             Console.WriteLine("-----------------------------------------------------------------------------");
             int sayi1, sayi2, toplam = 0;
-            Console.Write("Birinci sayıyı giriniz:");
+            Console.Write("Enter the first number:");
             sayi1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Birinci sayıyı giriniz:");
+            Console.Write("Enter the second number:");
             sayi2 = Convert.ToInt32(Console.ReadLine());
 
             if (sayi2 > sayi1)
             {
                 for (int a = sayi1; a < sayi2; a++)
                 {
-
                     toplam = toplam + a;
-
                 }
             }
             else
             {
                 for (int a = sayi2; a < sayi1; ++a)
                 {
-
                     toplam = toplam + a;
-
                 }
             }
-
-
-            Console.Write("Girdiğiniz sayılar arasında kalan sayıların toplamı:" + toplam);
+            Console.Write("The sum of the numbers remaining between the numbers you entered:" + toplam);
             Console.ReadKey();
-
-
-
-
         }
     }
 }
