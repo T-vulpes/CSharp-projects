1f1b1c6b122887dd0e180fc4ba71eef1b250776a
Deniz <167799743+T-vulpes@users.noreply.github.com>
2024-10-07 13:34:56 +0300

    Program.cs

--- finding factorial/ConsoleApplication6/Program.cs
+++ finding factorial/ConsoleApplication6/Program.cs
@@ -9,16 +9,16 @@ namespace ConsoleApplication6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("klavyeden girilen sayının faktöriyel cinsinden bulan program!");
+            Console.WriteLine("Program that finds the factorial of a number entered from the keyboard!");
             Console.WriteLine("-----------------------------------------------------------");
             int i, sayi, fakt = 1;
-            Console.Write("Sayı giriniz: ");
+            Console.Write("Enter a number: ");
             sayi = Convert.ToInt32(Console.ReadLine());
             for (i = 1; i <= sayi; i++)
             {
                 fakt = fakt * i;
             }
-            Console.Write("Sonuç:" + fakt);
+            Console.Write("Result:" + fakt);
             Console.ReadKey();
         }
     }
